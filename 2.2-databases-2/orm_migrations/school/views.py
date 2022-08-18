8f8c6021fc1cc01c87bd537b953582b01f4baef0
AngelaLoiko <angelaloiko@gmail.com>
2022-08-18 14:59:55 +0200

    all

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -5,14 +5,14 @@ from .models import Student
 
 
 def students_list(request):
+    ordering = 'group'
     template = 'school/students_list.html'
-    students = Student.objects.all().order_by('group')
+    students = Student.objects.all().order_by(ordering)
     context = {'students': students}
 
     #context = {}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
 
     return render(request, template, context)
