cf73b67c19a678d3d442b47291d771e964d5c184
AngelaLoiko <angelaloiko@gmail.com>
2022-08-26 19:55:40 +0200

    crud

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,25 @@
 from rest_framework import serializers
+from .models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -26,7 +31,8 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for element in positions:
+            StockProduct.objects.create(stock=stock, **element)
         return stock
 
     def update(self, instance, validated_data):
@@ -39,5 +45,18 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        positions_instance = instance.positions.all()
+
+        for rec in positions_instance:
+            for w in positions:
+                if rec.product == w['product']:
+                    if 'price' in w:
+                        rec.price = w['price']
+                    if 'quantity' in w:
+                        rec.quantity = w['quantity']
+                    if rec.product.id != w['product'].id:
+                        StockProduct.objects.create(product=w['product'], price=w['price'], quantity=w['quantity'],
+                                                    stock_id=rec.stock_id)
+                        rec.save()
 
         return stock
